170e0f2c44fa840f9ca5dbdd10bc0c1ecc14b769
lopezloo <lopez@multitheftauto.pl>
2017-01-21 14:18:05 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pyfilmweb',
+      version='0.1',
+      description='Python package for Filmweb unofficial mobile API',
+      author='lopezloo',
+      url='https://github.com/lopezloo/pyfilmweb',
+      packages=['filmweb'],
+     )
