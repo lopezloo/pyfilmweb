dabd772e77d2e0ab668cd37479d41fcbf0d8e564
lopezloo <lopez@multitheftauto.pl>
2018-02-10 10:31:32 +0100

    Bump version (0.1.1.2)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pyfilmweb',
-      version='0.1.1.1',
+      version='0.1.1.2',
       description='Python package for Filmweb unofficial mobile API',
       author='lopezloo',
       url='https://github.com/lopezloo/pyfilmweb',
